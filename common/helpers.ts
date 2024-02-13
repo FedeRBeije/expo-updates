511ca1699b7aaf260d6a55c5e3c16853e704a647
Federico Reghini <federicoreghini@Federicos-Air.station>
2024-02-13 12:41:45 +0100

    log

--- common/helpers.ts
+++ common/helpers.ts
@@ -89,10 +89,6 @@ export async function getAssetMetadataAsync(arg: GetAssetMetadataArg) {
   const key = createHash(asset, 'md5', 'hex');
   const keyExtensionSuffix = arg.isLaunchAsset ? 'bundle' : arg.ext;
   const contentType = arg.isLaunchAsset ? 'application/javascript' : mime.getType(arg.ext);
-  console.log(
-    'url',
-    `${process.env.NEXT_PUBLIC_HOSTNAME}/api/assets?asset=${assetFilePath}&runtimeVersion=${arg.runtimeVersion}&platform=${arg.platform}`
-  );
 
   return {
     hash: assetHash,
